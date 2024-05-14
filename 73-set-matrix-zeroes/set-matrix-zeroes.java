b123e0929d246da53588bb91b4a20f49cb51081b
Adnan Habib <98457152+ErAdnanHabib@users.noreply.github.com>
2024-05-14 18:08:24 +0530

    Time: 1 ms (73.35%) | Memory: 45.5 MB (38.24%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean hasRowZero=false;
+        boolean hasColZero=false;
+        
+        for(int c=0;c<matrix[0].length;c++){
+            if(matrix[0][c]==0){
+                hasRowZero=true; 
+                break;  
+            }
+        }
+        for(int r=0;r<matrix.length;r++){
+            if(matrix[r][0]==0){
+                hasColZero=true;
+                break;
+            }
+        }
+        // ----------------------------------------------
+        for(int r=1;r<matrix.length;r++){
+            for(int c=1;c<matrix[0].length;c++){
+                if(matrix[r][c]==0){
+                    matrix[r][0]=0;
+                    matrix[0][c]=0;
+                }
+            }
+        }
+        // ----------------------------------------------
+        // now we will nullify the whole row and column
+
+        for(int r=1;r<matrix.length;r++){
+            if(matrix[r][0]==0){
+                nullifyRow(matrix,r);
+            }
+        }
+        for(int c=1;c<matrix[0].length;c++){
+            if(matrix[0][c]==0){
+                nullifyCol(matrix,c);
+            }
+        }
+        if(hasRowZero) nullifyRow(matrix,0);
+        if(hasColZero) nullifyCol(matrix,0);
+    }
+
+    public void nullifyRow(int[][] matrix, int row){
+        for(int c=0;c<matrix[0].length;c++){
+            matrix[row][c]=0;
+        }
+    }
+    public void nullifyCol(int[][] matrix, int col){
+        for(int r=0;r<matrix.length;r++){
+            matrix[r][col]=0;
+        }
+    }
+}
