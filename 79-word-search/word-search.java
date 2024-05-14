4dd612ba7b747ce737c1f4a09a727b378e3d3154
Adnan Habib <98457152+ErAdnanHabib@users.noreply.github.com>
2024-05-14 18:38:56 +0530

    Time: 130 ms (83.57%) | Memory: 41.7 MB (31.50%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-
         for(int r=0;r<board.length;r++){
             for(int c=0;c<board[0].length;c++){
                 if(dfs(board,word,r,c,0))return true;
             }
         } return false;
     }
-
     public boolean dfs(char[][] board, String word, int row, int col, int curr){
-
         if(word.length()==curr)return true;
         if(row<0 || row>=board.length || col<0 || col>=board[0].length || board[row][col]=='#')return false;
         char ch =board[row][col];
