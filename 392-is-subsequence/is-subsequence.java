8978ef48775b6314f506b0a6cecef98d091532af
Adnan Habib <98457152+ErAdnanHabib@users.noreply.github.com>
2024-06-08 16:57:12 +0530

    Time: 2 ms (61.79%) | Memory: 41.3 MB (44.44%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        boolean answer=false;
+        Queue<Character> queue=new LinkedList<>();
+        for(int i=0;i<s.length();i++){
+            queue.add(s.charAt(i));
+        }
+        for(int i=0 ; i<t.length() && !queue.isEmpty() ; i++){
+            if(queue.peek() == t.charAt(i))queue.poll();
+        }
+        
+        return queue.isEmpty();
+    }
+}
