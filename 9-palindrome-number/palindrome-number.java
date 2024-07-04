95bbccaf67b77dd011f01433c088f17100dced60
Adnan Habib <98457152+ErAdnanHabib@users.noreply.github.com>
2024-07-05 04:26:56 +0530

    Time: 6 ms (35.77%) | Memory: 43.7 MB (79.08%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String str=Integer.toString(x);
+        StringBuilder str=new StringBuilder();
+        str.append(x);
         int left=0;
         int right=str.length()-1;
         while(left<=right){
