c21dd601deb20414edffafc9df7c20e1cc2c44f3
Adnan Habib <98457152+ErAdnanHabib@users.noreply.github.com>
2024-07-10 00:46:50 +0530

    Time: 7 ms (29.25%) | Memory: 46.3 MB (17.00%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int result=0;
+        for(int num:nums){
+            result ^=num;
+        }
+        return result;
+        
+    }
+}
