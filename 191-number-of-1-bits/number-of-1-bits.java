ed70ca67fe7eed327fa96ff5dc0f22f58a432879
Adnan Habib <98457152+ErAdnanHabib@users.noreply.github.com>
2024-07-09 23:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (74.34%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,13 +1,12 @@
-public class Solution {
-    // you need to treat n as an unsigned value
+class Solution {
     public int hammingWeight(int n) {
-        int count=0;
-        for(int i=0;i<32;i++){
+        int counts=0;
+        for(int i=1 ; i<=32 ;i++){
             int musk=1<<i;
             if((n & musk)!=0){
-                count++;
+                counts++;
             }
-        }return count;
-        
+        }
+        return counts;   
     }
 }
