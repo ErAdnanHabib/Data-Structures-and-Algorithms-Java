da3ed15dafafa19b7a8b72c16a410ca1de7a2132
Adnan Habib <98457152+ErAdnanHabib@users.noreply.github.com>
2024-08-04 14:37:10 +0530

    Time: 132 ms (81.68%) | Memory: 41.1 MB (96.39%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,22 +1,32 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int r=0;r<board.length;r++){
-            for(int c=0;c<board[0].length;c++){
-                if(dfs(board,word,r,c,0))return true;
+        if(board==null || board.length==0)return false;
+
+        for(int i = 0 ; i<board.length ; i++){
+            for(int j = 0 ; j<board[0].length ; j++){
+                if(dfs(board , i , j , word , 0)){
+                    return true;
+                }
             }
-        } return false;
+        }return false;
     }
-    public boolean dfs(char[][] board, String word, int row, int col, int curr){
-        if(word.length()==curr)return true;
-        if(row<0 || row>=board.length || col<0 || col>=board[0].length || board[row][col]=='#')return false;
-        char ch =board[row][col];
-        if(word.charAt(curr) != ch)return false;
-        board[row][col]='#';
-        boolean top= dfs(board,word,row-1,col,curr+1);
-        boolean left= dfs(board,word,row,col-1,curr+1);
-        boolean bottom= dfs(board,word,row+1,col,curr+1);
-        boolean right= dfs(board,word,row,col+1,curr+1);
-        board[row][col]=ch;
-        return top || left || right || bottom;
+    public boolean dfs(char[][] board ,int i , int j , String word , int k){
+        if(i<0 || i >=board.length || j<0 || j >=board[0].length || board[i][j] != word.charAt(k)){
+            return false;
+        }
+        if(k==word.length()-1)return true;
+
+        char temp = board[i][j];
+
+        board[i][j] = '$';
+
+        boolean found = dfs(board , i-1 , j , word , k+1) ||
+                        dfs(board , i+1 , j , word , k+1) ||
+                        dfs(board , i , j-1 , word , k+1) ||
+                        dfs(board , i , j+1 , word , k+1) ;
+            
+        board[i][j] = temp;
+
+        return found;
     }
 }
