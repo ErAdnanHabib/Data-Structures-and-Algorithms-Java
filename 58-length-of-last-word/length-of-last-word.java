3e514d1ac27e9a643d71e00173a0bc46294a3005
Adnan Habib <98457152+ErAdnanHabib@users.noreply.github.com>
2024-09-05 12:22:06 +0530

    Time: 1 ms (33.46%) | Memory: 42.2 MB (6.82%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,12 +1,9 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr= s.split(" ");
-        int len=arr.length-1;
+
+        String arr[] = s.split(" ");
+        int len = arr.length-1;
         return arr[len].length();
         
-        
-        
-        
-
     }
 }
